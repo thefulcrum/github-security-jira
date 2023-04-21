6e4db10cb6a58800aaac7826c6b7561020027d35
Tim Molloy <tim.molloy@n3hub.com>
2023-04-21 16:34:41 +1200

    test

--- src/SecurityAlertIssue.php
+++ src/SecurityAlertIssue.php
@@ -175,10 +175,10 @@ EOT;
 
         var_dump($severity);
 
-        var_dump([["set"=>[["name"=>$severity]]]]);
+        var_dump([["value" => $severity]]);
 
         // Set Severity customfield_11633 
-        $issueField->addCustomField("customfield_11633", [["set"=>[["value"=>$severity]]]]);
+        $issueField->addCustomField("customfield_11633", [["value" => $severity]]);
 
         
         try {
